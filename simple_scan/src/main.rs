f9db712d88d3c77243ca2d3762e290e812b66eb4
Macha-K <macha.snoopy@gmail.com>
2020-11-07 16:10:29 +0900

    file input

--- simple_scan/src/main.rs
+++ simple_scan/src/main.rs
@@ -2,6 +2,9 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
 fn main() -> Result<(), Box<std::error::Error>> {
-    for result in BufReader::new(File::open("")
-
+    for result in BufReader::new(File::open("/home/kmasa/seccamp2020/dataset/rm/03ec5e176ea404f1193608a4298a5ebdaa2e275461836b6762d25cf19b252446")?).lines() {
+        let l = result?;
+        println!("{}", l);
+    }
+    Ok(())
 }
