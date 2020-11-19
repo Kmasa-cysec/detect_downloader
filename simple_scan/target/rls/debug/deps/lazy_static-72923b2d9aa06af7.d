c09100a8318cd0979bf6d676140a69daa2248bcf
Kmasa-cysec <kmasa.cysec@gmail.com>
2020-11-19 21:13:06 +0900

    fixed

---
+++ simple_scan/target/rls/debug/deps/lazy_static-72923b2d9aa06af7.d
@@ -0,0 +1,6 @@
+/home/kmasa/seccamp2020/rust-antimalware/simple_scan/target/rls/debug/deps/lazy_static-72923b2d9aa06af7.rmeta: /home/kmasa/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /home/kmasa/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
+
+/home/kmasa/seccamp2020/rust-antimalware/simple_scan/target/rls/debug/deps/lazy_static-72923b2d9aa06af7.d: /home/kmasa/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /home/kmasa/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
+
+/home/kmasa/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs:
+/home/kmasa/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs:
---
+++ simple_scan/target/rls/debug/deps/simple_scan-a5b4dfc17c08cf23.d
@@ -0,0 +1,5 @@
+/home/kmasa/seccamp2020/rust-antimalware/simple_scan/target/rls/debug/deps/simple_scan-a5b4dfc17c08cf23.rmeta: src/main.rs
+
+/home/kmasa/seccamp2020/rust-antimalware/simple_scan/target/rls/debug/deps/simple_scan-a5b4dfc17c08cf23.d: src/main.rs
+
+src/main.rs:
