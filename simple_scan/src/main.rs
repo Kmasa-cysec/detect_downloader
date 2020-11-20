fd67fbc09d0c023086395d6711e0d600a0183d43
Kmasa-cysec <kmasa.cysec@gmail.com>
2020-11-20 23:34:31 +0900

    scan directory/file(simple_match)

--- simple_scan/src/main.rs
+++ simple_scan/src/main.rs
@@ -6,30 +6,29 @@ use std::io::prelude::*;
 extern crate regex;
 use regex::Regex;
 
-// "/home/kmasa/seccamp2020/dataset/rm/03ec5e176ea404f1193608a4298a5ebdaa2e275461836b6762d25cf19b252446")
+// "~/dataset/rm/03ec5e176ea404f1193608a4298a5ebdaa2e275461836b6762d25cf19b252446")
 
-//const SIGN: &str = "wget http://";
-//const SIGN = Regex::new(r"wget http://+;").unwrap();
-//
 fn check_dir(target_path: &str) -> bool {
     let check = metadata(target_path).unwrap();
     check.is_dir()
 }
 
-fn search_dir(target_path: &str) -> u8 {
+fn search_dir(target_path: &str, mut scanfile_count: u8) -> u8 {
     let paths = fs::read_dir(target_path).unwrap();
     let mut detected_count_temp: u8 = 0;
     let mut return_detected_count: u8 = 0;
     for path in paths {
         let path_str: &str = &(path.unwrap().path().display()).to_string();
         if check_dir(path_str) {
-            search_dir(path_str);
+            search_dir(path_str, scanfile_count);
         } else {
             println!("Scan {}", path_str);
             return_detected_count = simple_scan_file(path_str);
+            scanfile_count = scanfile_count + 1;
         }
         detected_count_temp = detected_count_temp + return_detected_count;
     }
+    println!("\n-----> Scan {} files", scanfile_count);
     detected_count_temp
 }
 
@@ -60,24 +59,20 @@ fn find_keywords(content: &str) -> u8 {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let target_path = &args[1];
-    //let filename = "/home/kmasa/seccamp2020/dataset/rm/03ec5e176ea404f1193608a4298a5ebdaa2e275461836b6762d25cf19b252446";
+    let mut init_scanfile_count = 0;
+    //let filename = "~/dataset/rm/03ec5e176ea404f1193608a4298a5ebdaa2e275461836b6762d25cf19b252446";
     println!("In file/directory {}", target_path);
 
     let detected_count: u8;
     if check_dir(target_path) {
-        detected_count = search_dir(target_path);
+        detected_count = search_dir(target_path, init_scanfile_count);
     } else {
         println!("Scan {}", target_path);
         detected_count = simple_scan_file(target_path);
+        init_scanfile_count = 1;
+        println!("\n-----> Scan {} file", init_scanfile_count);
     }
-    println!("Detected count is {}", detected_count);
+    println!("-----> Detected {} files", detected_count);
     //    assert!(contents.contains("wget"));
     //    matched(&contents);
 }
-
-/*
-fn matched(content: &str) {
-    println!("debug");
-    assert!(content.contains(SIGN));
-}
-*/
